5944103af71ccb0e14e1eadb3604041734f8a0b9
jdll1213 <66875069+jdll1213@users.noreply.github.com>
2025-02-25 20:19:25 +0000

    exercise 6 done

---
+++ cin-ex6.cpp
@@ -0,0 +1,16 @@
+// Working with memory addresses and pointers
+
+#include <iostream>
+using namespace std;
+
+int y;
+int *x;
+
+int main() {
+
+    y = 10;
+    x = &y;
+    cout << x << "\n";
+    y = 20;
+    cout << x << "\n";
+}
