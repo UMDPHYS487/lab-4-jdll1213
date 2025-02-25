d558063dc4591996935159bb7db18ee7fd621e60
jdll1213 <66875069+jdll1213@users.noreply.github.com>
2025-02-25 19:18:11 +0000

    exercise 2 done

--- Loop-ex2.cpp
+++ Loop-ex2.cpp
@@ -1,1 +1,8 @@
-// add your code here
+// Create a for loop that prints the current count
+
+#include <iostream>
+
+int main() {
+    for (int i = 1; i < 11; ++i)
+    std::cout << "Loop count is " << i << "\n";
+}
