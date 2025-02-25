9366ad90415b03ce89e1ebc0d6f82dc503ea7434
jdll1213 <66875069+jdll1213@users.noreply.github.com>
2025-02-25 20:49:47 +0000

    exercise 7 done

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,40 @@
-// add your code here
+// Code example with switch case
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    cout << "Please enter an integer value: ";
+    cin >> i;
+
+    if (i%2 == 1) {
+
+        cout << i/2;
+    }
+    
+    if (i%2 == 0) {
+
+        switch (i) {
+
+            case 2:
+                cout << 2*2;
+                break;
+            case 4:
+                cout << 4*4;
+                break;
+            case 6:
+                cout << 6*6;
+                break;
+            case 8:
+                cout << 8*8;
+                break;
+            
+            default:
+                cout << "hahaha";
+    
+        }
+
+    }
+
+}
