6a8925479832d1831c74ba7c477a97a3c1274f6f
jdll1213 <66875069+jdll1213@users.noreply.github.com>
2025-02-25 19:51:28 +0000

    exercise 5 done

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,22 @@
-// Add your code here
+// Create a user input field and then calculate the factorial
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    int k = 1;
+    while (n > 1) {
+        k = k*n;
+        --n;    
+    }
+    return k;
+}
+
+int main() {
+
+    int i;
+    cout << "Please enter an integer value: " ;
+    cin >> i;
+    cout << "The value you entered is " << i << " and its factorial is " << factorial(i) << "\n";
+
+}
