9e75292969a5c72691e1ec61c428a0715b9940bb
jdll1213 <66875069+jdll1213@users.noreply.github.com>
2025-02-25 21:11:02 +0000

    exercise 8 done

--- Bitwise-ex8.cpp
+++ Bitwise-ex8.cpp
@@ -1,1 +1,16 @@
-// Your code here
+// Bitwise operations
+
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+
+int main() {
+    bitset<8> b{0b01110010};
+        cout << b << " (initial value)\n";
+
+        b.flip();
+
+        cout << b << " (final value)\n";
+
+}
